1549bebe7039c40fd0d03aed7a1c20d56573a453
Jasur0999 <leaderjasur@gmail.com>
2024-02-05 18:44:45 +0500

    afver

--- main.js
+++ main.js
@@ -1,8 +1,66 @@
-//  //////////////////////////////////
-// let arr = ['everest', 'fuji'];
-// console.log(arr[1][2]);
+// 1- task   //////////////////////////////////
 
-const fruist = ['Apple','Banana','Pear'];
-fruist[fruist.length] = 'Limon';
-fruist[7] = 'Orange';
-console.log(fruist);
+// let arr = ['car', 'home', 'three', 'it'];
+// console.log(arr);
+
+// 2- task   //////////////////////////////////
+
+// function geneArray(n, A, B) {
+//     var arr = [A, B];
+//     for (var i = 2; i < n; i++) {
+//       arr[i] = arr[i-1] + arr[i-2];
+//     }
+//     return arr;
+//   }
+
+//   var n = 5;
+//   var A = 2;
+//   var B = 3;
+//   console.log(geneArray(n, A, B));
+
+// 3- task   //////////////////////////////////
+
+// var lone = ['Kia', 'Lambargini', 'Rangerove', 'Chevrolet'];
+// lone.reverse();
+// console.log(lone);
+
+// 4- task   //////////////////////////////////
+
+// let odd = [4, 9, 7, 8, 6, 5];
+// let s = 0;
+// for (let i = 0; i <= odd[i]; i++) {
+//   s++;
+//   if (odd[i] % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+// console.log(s, "ta toq son bor");
+
+// 5- task   //////////////////////////////////
+
+// var oddCouple = [4, 5, 7, 8, 1, 9];
+// let sum = 0;
+// let odd = 0;
+// for (let i = 0; i <= oddCouple.length; i++) {
+//   if (oddCouple[i] % 2 == 0) {
+//     sum++;
+//     console.log(oddCouple[i]);
+//   } else if (oddCouple[i] % 2 == 1){
+//     odd++;
+//     console.log(oddCouple[i]);
+//   }
+// }
+// console.log(sum, "ta juft son", odd, 'ta toq son');
+
+// 6- task   //////////////////////////////////
+
+// function chiqarJuftElement(arr) {
+//   for (let i = 1; i < arr.length; i += 2) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8];
+// chiqarJuftElement(array);
+
+// 7- task   //////////////////////////////////
